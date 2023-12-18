b6f9090aad1fae4d3f56af2c15c2c7f8c5a01d4e
Evelina Neumann <evelina.neumann1@gmail.com>
2023-12-18 20:02:23 +0100

    dealer's cards set up

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -13,16 +13,62 @@ public class BlackJack {
             this.value = value;
             this.type = type;
         }
+
+        public String toString (){
+            return value + "-" + type;
+        }
+        public int getValue () {
+            if ("AJQK".contains(value)){
+                if (value =="A"){
+                    return 11;//for A
+                }
+                return 10;//for J,K,Q Cards
+            }
+            return Integer.parseInt(value);//for 2 to 10 cards;
+        }
+
+        public boolean isAce(){
+            return value == "A";
+        }
     }
 
     ArrayList<Card> deck;
+    Random random = new Random();//to shuffle the deck
+    //dealer
+         Card hiddenCard;
+         ArrayList<Card> dealerHand;
+         int dealerSum;
+         int dealerAceCount;
+    
     BlackJack(){
         startGame();
     }
     public void startGame(){
         //a representation for a card
          buildDeck();
-    }
+         //shuffle the cards
+         shuffleDeck();
+         //dealer
+         dealerHand = new ArrayList<Card>();
+         dealerSum = 0;
+         dealerAceCount = 0;
+         
+         hiddenCard = deck.remove(deck.size()-1);//remove card at last index
+         dealerSum += hiddenCard.getValue();
+         dealerAceCount += hiddenCard.isAce() ? 1 : 0;
+
+         Card card = deck.remove(deck.size()-1);
+         dealerSum +=card.getValue();
+         dealerAceCount += card.isAce() ? 1 : 0;
+         dealerHand.add(card);
+
+
+         System.out.println("DEALER:");
+         System.out.println(hiddenCard);
+         System.out.println(dealerHand);
+         System.out.println(dealerSum);
+         System.out.println(dealerAceCount);
+     }
     public void buildDeck (){
         deck = new ArrayList<Card>();
         String[] values = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"}; 
@@ -37,5 +83,16 @@ public class BlackJack {
 
         System.out.println("BUILD DECK: ");
         System.out.println(deck);
+    }
+    public void shuffleDeck() {
+         for ( int i=0; i<deck.size();i++){
+            int j = random.nextInt(deck.size());
+            Card currCard = deck.get(i);
+            Card randomCard = deck.get(j);
+            deck.set(i, randomCard);
+            deck.set(j, currCard);
+         }
+         System.out.println("after Shuffle");
+         System.out.println(deck);
     }
 }
