df7b2947f2dc3fc243da9106382bec7acda7f6d3
Evelina Neumann <evelina.neumann1@gmail.com>
2023-12-19 13:45:03 +0100

    added player

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -39,6 +39,12 @@ public class BlackJack {
          ArrayList<Card> dealerHand;
          int dealerSum;
          int dealerAceCount;
+
+    //player
+    ArrayList<Card> playerHand;
+    int playerSum;
+    int playerAceCount;
+
     
     BlackJack(){
         startGame();
@@ -68,6 +74,23 @@ public class BlackJack {
          System.out.println(dealerHand);
          System.out.println(dealerSum);
          System.out.println(dealerAceCount);
+
+
+         //player
+         playerHand = new ArrayList<Card>();
+         playerSum = 0;
+         playerAceCount = 0; 
+
+         for (int i=0; i<2; i++){
+         card = deck.remove(deck.size()-1);
+         playerSum += card.getValue();
+         playerAceCount += card.isAce() ? 1 : 0;
+         playerHand.add(card);
+         }
+         System.out.println("PLAYER: ");
+         System.out.println(playerHand);
+         System.out.println(playerSum);
+         System.out.println(playerAceCount);
      }
     public void buildDeck (){
         deck = new ArrayList<Card>();
